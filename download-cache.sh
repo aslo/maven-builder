cd692762988eedac9daf1febac16caac6e7f6592
Alex Sloan <alexsloan@google.com>
2017-05-05 15:45:49 -0400

    stash

--- download-cache.sh
+++ download-cache.sh
@@ -17,9 +17,9 @@ mkdir -p docker/m2/repository
 pushd docker/m2/repository
 
 # run a build without any caching
-docker run -v $PROJECT_TO_BUILD:/workspace -w /workspace maven:3.5.0-jdk-8 mvn --batch-mode dependency:go-offline >> out.log
+docker run -v $PROJECT_TO_BUILD:/workspace -w /workspace maven:3.5.0-jdk-8 mvn --batch-mode dependency:resolve >> out.log
 
 # parse download URLs from log and fetch them from maven central
-grep -e "Downloaded\:.*\.jar" out.log | awk '{print $3}' | xargs wget -r -nH --cut-dirs=1 $line
+grep -e "Downloaded\:.*\.jar\|\.pom" out.log | awk '{print $3}' | xargs wget -r -nH --cut-dirs=1 $line
 
 popd
